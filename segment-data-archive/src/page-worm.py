7b3cd406c6a457e2d424ff46e59a5f33e386e75b
liuhuihao <1061036802@qq.com>
2018-11-08 19:19:47 +0800

    feat: segment-data-archive 分页方式

--- segment-data-archive/src/page-worm.py
+++ segment-data-archive/src/page-worm.py
@@ -1,5 +1,21 @@
 import requests
 from utils import initMysql, getByAid
+from pyquery import PyQuery as pq
 
-r = requests.get('https://segmentfault.com/blogs/newest?page=' + str(1500), timeout=10)
-print(r.text)
+connect = initMysql()
+
+
+def getUrl(i, element):
+    aid = pq(element).attr("href").split("/")[2]
+    getByAid(aid, connect)
+
+
+# 起始 和 终止 page
+startPage = 1
+endPage = 50
+
+for page in range(startPage, endPage):
+    print("----------- PAGE " + str(page) + " ------------")
+    r = requests.get('https://segmentfault.com/blogs/newest?page=' + str(page), timeout=10)
+    ht = pq(r.text)
+    ht(".stream-list__item h2 a").each(getUrl)
